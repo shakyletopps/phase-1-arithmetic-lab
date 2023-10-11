9bbba4d538d5a1f77e03092b8acc31fd978e32b0
+ <+>
2023-10-11 12:36:57 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = 2 * 31;
+const random = Math.ceil(Math.random());
+const num3 = 16 ;
+const num4 = 4 ;
+const mod = 16 / 4;
+const max = Math.max(1,2,3,4,5,20);
